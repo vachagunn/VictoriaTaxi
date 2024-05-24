dfc4b7192a787421d140718bd0ee61f1c3b8b69d
vachagunn <vachikpuper@gmail.com>
2024-05-24 11:08:25 +0500

    Добавление API для карты

--- js/main.js
+++ js/main.js
@@ -43,3 +43,34 @@ phoneInputs.forEach((input) => {
         if (input.value == '+') input.value = '';
     })
 });
+
+
+// Yandex Map
+initMap();
+
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
+
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [56.803569, 60.567156],
+
+                // Уровень масштабирования
+                zoom: 15
+            }
+        }
+    );
+
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+}
